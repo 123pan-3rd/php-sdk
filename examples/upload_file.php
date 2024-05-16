372a1cb8da034c954de17c00e00b8d2a2ebcc305
flyqie <flyqie@outlook.com>
2024-05-17 02:18:11 +0800

    更改格式

--- examples/upload_file.php
+++ examples/upload_file.php
@@ -1,1 +1,32 @@
-<?php/** * 该文件演示了如何上传大文件到123云盘 * * 使用前请先在bootstrap.php中配置相关信息 */use Pan123\Pan123;ini_set("memory_limit", "256M");require_once __DIR__ . "/../tests/bootstrap.php";$pan123 = new Pan123(PAN123_ACCESS_TOKEN, PAN123_CLIENT_ID, PAN123_CLIENT_SECRET, 0, false);// 生成测试文件$fileSize = 3333 * 1024 * 1024;$file = fopen(__DIR__ . "/test_big_file.txt", "w");$blockSize = 8192; // 8KB$numWrites = ceil($fileSize / $blockSize);for ($i = 0; $i < $numWrites; $i++) {	$randomContent = openssl_random_pseudo_bytes($blockSize);	fwrite($file, $randomContent);}fclose($file);try {	var_dump($pan123->fileUpload(0, "test_big_file.txt", fopen(__DIR__ . "/test_big_file.txt", "rb")));} catch (\Exception $e) {	echo "Failed: " . $e->getMessage() . PHP_EOL;} finally {	@unlink(__DIR__ . "/test_big_file.txt");}
+<?php
+/**
+ * 该文件演示了如何上传大文件到123云盘
+ *
+ * 使用前请先在bootstrap.php中配置相关信息
+ */
+
+use Pan123\Pan123;
+
+ini_set("memory_limit", "256M");
+require_once __DIR__ . "/../tests/bootstrap.php";
+
+$pan123 = new Pan123(PAN123_ACCESS_TOKEN, PAN123_CLIENT_ID, PAN123_CLIENT_SECRET, 0, false);
+
+// 生成测试文件
+$fileSize = 3333 * 1024 * 1024;
+$file = fopen(__DIR__ . "/test_big_file.txt", "w");
+$blockSize = 8192; // 8KB
+$numWrites = ceil($fileSize / $blockSize);
+for ($i = 0; $i < $numWrites; $i++) {
+	$randomContent = openssl_random_pseudo_bytes($blockSize);
+	fwrite($file, $randomContent);
+}
+fclose($file);
+
+try {
+	var_dump($pan123->fileUpload(0, "test_big_file.txt", fopen(__DIR__ . "/test_big_file.txt", "rb")));
+} catch (\Exception $e) {
+	echo "Failed: " . $e->getMessage() . PHP_EOL;
+} finally {
+	@unlink(__DIR__ . "/test_big_file.txt");
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,6 @@
-<?phprequire_once __DIR__ . "/../vendor/autoload.php";const PAN123_ACCESS_TOKEN = "";const PAN123_CLIENT_ID = "";const PAN123_CLIENT_SECRET = "";
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+const PAN123_ACCESS_TOKEN = "";
+const PAN123_CLIENT_ID = "";
+const PAN123_CLIENT_SECRET = "";
